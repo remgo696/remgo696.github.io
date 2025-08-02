5121f9b62b9c0055b822d78237faa4b6dd156a2b
remgo <37494165+dvladimirdm@users.noreply.github.com>
2025-08-01 22:46:12 -0500

    Remove base path: Site now works on root domain https://remgo696.github.io/

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://remgo696.github.io',
-  base: '/maxwell-soluciones',
   integrations: [
     tailwind({
       applyBaseStyles: false,
